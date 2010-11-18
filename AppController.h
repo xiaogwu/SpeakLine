0a21a08d7fb1b67b976dd9e54427d1e9c873b2af
Xiao G. Wu <xiaogwu@gmail.com>
2010-11-17 20:55:03 -0800

    Added voice selection

--- AppController.h
+++ AppController.h
@@ -9,12 +9,15 @@
 #import <Cocoa/Cocoa.h>
 
 
-@interface AppController : NSObject <NSSpeechSynthesizerDelegate> {
+@interface AppController : NSObject <NSSpeechSynthesizerDelegate, NSTableViewDelegate> {
 	IBOutlet NSTextField *textField;
 	NSSpeechSynthesizer *speechSynth;
 	
 	IBOutlet NSButton *stopButton;
 	IBOutlet NSButton *startButton;
+	
+	IBOutlet NSTableView *tableView;
+	NSArray *voiceList;
 
 }
 - (IBAction)sayIt:(id)sender;
--- AppController.m
+++ AppController.m
@@ -18,7 +18,16 @@
 	
 	speechSynth = [[NSSpeechSynthesizer alloc] initWithVoice:nil];
 	[speechSynth setDelegate:self];
+	voiceList = [[NSSpeechSynthesizer availableVoices] retain];
 	return self;
+}
+
+- (void)awakeFromNib {
+	NSString *defaultVoice = [NSSpeechSynthesizer defaultVoice];
+	int defaultRow = [voiceList indexOfObject:defaultVoice];
+	[tableView selectRowIndexes:[NSIndexSet indexSetWithIndex:defaultRow] 
+		   byExtendingSelection:NO];
+	[tableView scrollRowToVisible:defaultRow];
 }
 
 - (IBAction)sayIt:(id)sender {
@@ -32,6 +41,7 @@
 	NSLog(@"Have started to say: %@", string);
 	[stopButton setEnabled:YES];
 	[startButton setEnabled:NO];
+	[tableView setEnabled:NO];
 }
 
 - (IBAction)stopIt:(id)sender {
@@ -39,11 +49,36 @@
 	[speechSynth stopSpeaking];
 }
 
-- (void)speechSynthesizer:(NSSpeechSynthesizer *)sender
+// Delegate Methods
+
+- (void)speechSynthesizer:(NSSpeechSynthesizer *)sender 
 		didFinishSpeaking:(BOOL)complete {
 	NSLog(@"complete = %d", complete);
 	[stopButton setEnabled:NO];
 	[startButton setEnabled:YES];
+	[tableView setEnabled:YES];
+}
+
+- (int)numberOfRowsInTableView:(NSTableView *)tv {
+	return [voiceList count];
+}
+
+- (id)tableView:(NSTabView *)tv 
+	objectValueForTableColumn:(NSTableColumn *)tableColumn 
+					      row:(int)row {
+	NSString *v = [voiceList objectAtIndex:row];
+	NSDictionary *dict = [NSSpeechSynthesizer attributesForVoice:v];
+	return [dict objectForKey:NSVoiceName];
+}
+
+- (void)tableViewSelectionDidChange:(NSNotification *)notification {
+	int row = [tableView selectedRow];
+	if (row == -1) {
+		return;
+	}
+	NSString *selectedVoice = [voiceList objectAtIndex:row];
+	[speechSynth setVoice:selectedVoice];
+	NSLog(@"new voice = %@", selectedVoice);
 }
 
 @end
